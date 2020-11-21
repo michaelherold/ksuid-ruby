289e40b5a21c7564d7fec270fc048f8b053b2f26
Michael Herold <opensource@michaeljherold.com>
2020-11-20 22:34:33 -0600

    Add doctests to CI
    Making sure that our documentation passes its tests is important to make
    sure it doesn't drift out-of-sync with the implementation.

--- spec/doctest_helper.rb
+++ spec/doctest_helper.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE'] || ENV['CI']
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+
+  SimpleCov.command_name 'yard-doctest'
+
+  YARD::Doctest.after_run do
+    SimpleCov.set_exit_exception
+    SimpleCov.run_exit_tasks!
+  end
+end
+
 require 'ksuid'
